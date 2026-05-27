ca36f652bb6821ac42c114198ed5ad5a1d04ec88
Feiro <Feirobot@gmail.com>
2026-05-28 02:44:40 +0800

    Add LivePhoto component with MDX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://openfilm.cc",
-  output: "static"
+  output: "static",
+  integrations: [mdx()]
 });
